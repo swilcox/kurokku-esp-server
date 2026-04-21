617c1a4daa27c089cd84f7e524317b9cd9f6b6b7
Steven Wilcox <steven@wilcoxzone.com>
2026-04-21 17:38:16 -0500

    Update version.go

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ import "runtime/debug"
 
 // Version is the server release displayed in the admin UI. Release builds can
 // override it with: -ldflags "-X github.com/swilcox/kurokku-esp-server/internal/version.Version=v1.2.3"
-var Version = "0.1.1-dev"
+var Version = "0.1.2"
 
 func ServerVersion() string {
 	if Version != "" && Version != "devel" {
